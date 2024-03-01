a2b6e8e48cf14a78f4648a57fb31f60f50e992fb
geonpeter <igeonpeter@gmail.com>
2024-03-01 18:11:58 +0000

    ram free space check project

---
+++ ram_monitoring.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+free_space=$(free -mt | grep "Total" | awk '{print $4}')
+threshold=1024
+
+if [[ $free_space -lt $threshold ]]
+then
+	echo "Warning, RAM is running low [$free_space] MB!"
+else
+	echo "RAM space is sufficient"
+fi
+
